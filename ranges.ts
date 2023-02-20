678abf8b5a8baf06db681b8f539b0e1f845d646f
Saro Amirkhanyan <saroamirkhanyan@Saros-MacBook-Air.local>
2023-02-21 00:56:54 +0400

    Created ranges library

---
+++ ranges.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+export function* range(start: number, end: number): Iterable<number> {
+    for (let i = start; i <= end; i++) yield i;
+}
+
+export function filter<A>(p: (a: A) => boolean) {
+    return function* (iterable: Iterable<A>) {
+        for (const a of iterable) {
+            if (p(a)) yield a;
+        }
+    };
+}
+
+export function map<A, B>(f: (a: A) => B) {
+    return function* (iterable: Iterable<A>) {
+        for (const a of iterable) yield f(a);
+    };
+}
